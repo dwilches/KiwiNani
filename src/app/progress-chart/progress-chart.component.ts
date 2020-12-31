00ad88d780e454a51e6b0257ce09e41cc787bbc4
Daniel Wilches <dwilches@gmail.com>
2020-12-31 13:31:08 -0500

    Add burned at

--- src/app/progress-chart/progress-chart.component.ts
+++ src/app/progress-chart/progress-chart.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from "@angular/core"
+import { AfterViewInit, Component, OnInit } from "@angular/core"
 import { WanikaniService } from "../wanikani.service/wanikani.service"
 import { Chart } from "chart.js"
 import * as moment from "moment"
@@ -6,14 +6,12 @@ import { Moment } from "moment"
 import * as _ from "lodash"
 
 type Level = { date: Moment, level: number }
-type Assignment = { date: Moment, total: number, kanjis: number, radicals: number, vocabulary: number }
+type Assignment1 = { date: Moment, total: number, kanjis: number, radicals: number, vocabulary: number }
 type AssignmentAccumulator = { total: number, radicals: number, kanjis: number, vocabulary: number }
 
-enum ChartTimeSpan {
-  All = "ALL",
-  LastMonth = "LAST_MONTH",
-  Last3Months = "LAST_3_MONTHS"
-}
+type ChartTimeSpan = "ALL" | "LAST_MONTH" | "LAST_3_MONTHS"
+type ChartCriteria = "LEVEL" | "PASSED_AT" | "BURNED_AT"
+
 
 @Component({
   selector: "app-progress-dashboard",
@@ -22,9 +20,12 @@ enum ChartTimeSpan {
 })
 export class ProgressChartComponent implements OnInit, AfterViewInit {
 
-  private levels: Level[]
+  private allLevels: Level[]
+  private allAssignments: Assignment[]
+
   private chart: Chart
-  currentTimeSpan: ChartTimeSpan
+  currentTimeSpan: ChartTimeSpan = "LAST_MONTH"
+  currentCriteria: ChartCriteria = "PASSED_AT"
   loading = false
 
   constructor(private wanikani: WanikaniService) {
@@ -36,47 +37,55 @@ export class ProgressChartComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    Promise.all([ this.getLevels(), this.getAssignments() ])
-      .then(([ alLevels, allAssignments ]) => {
-        this.levels = alLevels
-
-        // This dataset has in "x" the date in which each level was reached, and in "y" the level number
-        const levelDataset = alLevels.map(({ level, date }) => ({ x: date, y: level }))
-
-        // This dataset has in "x" the date in which an assignment was passed, and in "y" the accumulated number of assignments passed
-        // up to that day.
-        const assignments = _.uniqBy(allAssignments, ass => moment(ass.date).startOf("hour").format())
-        const kanjisDataset = assignments.map(({ kanjis, date }) => ({ x: date, y: kanjis }))
-        const vocabularyDataset = assignments.map(({ vocabulary, date }) => ({ x: date, y: vocabulary }))
-        const radicalsDataset = assignments.map(({ radicals, date }) => ({ x: date, y: radicals }))
-
-        this.chart = this.createChart(levelDataset, radicalsDataset, kanjisDataset, vocabularyDataset)
-        this.changeTimeSpan(ChartTimeSpan.LastMonth)
-        this.loading = false
+    Promise.all([ this.getLevels(), this.wanikani.getAllAssignments() ])
+      .then(([ allLevels, allAssignments ]) => {
+        this.allLevels = allLevels
+        this.allAssignments = allAssignments
+        this.regenerateChartData()
       })
       .catch(error => {
-        this.loading = false
         console.log(error)
       })
+      .finally(() => this.loading = false)
   }
 
-  public changeTimeSpan(timespan: string): void {
-    this.currentTimeSpan = timespan as ChartTimeSpan
+  private regenerateChartData(): void {
+    // This dataset has in "x" the date in which each level was reached, and in "y" the level number
+    const levelDataset = this.allLevels.map(({ level, date }) => ({ x: date, y: level }))
 
+    // This dataset has in "x" the date in which an assignment was passed, and in "y" the accumulated number of assignments passed
+    // up to that day.
+    const assignments = _.uniqBy(this.getAssignments(), ass => moment(ass.date).startOf("hour").format())
+    const kanjisDataset = assignments.map(({ kanjis, date }) => ({ x: date, y: kanjis }))
+    const vocabularyDataset = assignments.map(({ vocabulary, date }) => ({ x: date, y: vocabulary }))
+    const radicalsDataset = assignments.map(({ radicals, date }) => ({ x: date, y: radicals }))
+
+    this.chart = this.createChart(levelDataset, radicalsDataset, kanjisDataset, vocabularyDataset)
+    this.changeTimeSpan(this.currentTimeSpan)
+  }
+
+  public changeTimeSpan(timespan: ChartTimeSpan): void {
     let min
 
-    switch (this.currentTimeSpan) {
-      case ChartTimeSpan.LastMonth:
+    switch (timespan) {
+      case "LAST_MONTH":
         min = moment().subtract(1, "months").startOf("day")
         break
-      case ChartTimeSpan.Last3Months:
+      case "LAST_3_MONTHS":
         min = moment().subtract(3, "months").startOf("day")
         break
-      case ChartTimeSpan.All:
-        min = this.levels[0].date.startOf("day")
+      case "ALL":
+        min = this.allLevels[0].date.startOf("day")
     }
 
+    this.currentTimeSpan = timespan
     this.chart.options.scales.xAxes[0].time = { min }
+    this.chart.update()
+  }
+
+  public changeCriteria(criteria: ChartCriteria): void {
+    this.currentCriteria = criteria
+    this.regenerateChartData()
     this.chart.update()
   }
 
@@ -88,7 +97,7 @@ export class ProgressChartComponent implements OnInit, AfterViewInit {
       })
   }
 
-  private getAssignments(): Promise<Assignment[]> {
+  private getAssignments(): Assignment1[] {
 
     function makeAccum(total: number, kanjis: number, radicals: number, vocabulary: number): AssignmentAccumulator {
       return { total, kanjis, radicals, vocabulary }
@@ -103,92 +112,121 @@ export class ProgressChartComponent implements OnInit, AfterViewInit {
       }
     }
 
-    return this.wanikani.getAllAssignments()
-      .then(assignments => {
-          const dateAndType: Array<{ date: string, type: string }> = _.sortBy(assignments, "passed_at")
-            .filter(ass => ass.passed_at)
-            .map(ass => ({
-              date: moment(ass.passed_at).startOf("hour").format(),
-              type: ass.subject_type
-            }))
+    const assignments = this.allAssignments
+    const field = this.currentCriteria === "PASSED_AT" ? "passed_at" : "burned_at"
+    const dateAndType: Array<{ date: string, type: string }> = _.sortBy(assignments, field)
+      .filter(ass => ass[field])
+      .map(ass => ({
+        date: moment(ass[field]).startOf("hour").format(),
+        type: ass.subject_type
+      }))
 
-          const accumByDate: Record<string, AssignmentAccumulator> = {}
-          dateAndType.reduce((prevAccum, curr) => {
-            if (!accumByDate[curr.date]) {
-              accumByDate[curr.date] = makeAccum(0, 0, 0, 0)
-            }
+    const accumByDate: Record<string, AssignmentAccumulator> = {}
+    dateAndType.reduce((prevAccum, curr) => {
+      if (!accumByDate[curr.date]) {
+        accumByDate[curr.date] = makeAccum(0, 0, 0, 0)
+      }
 
-            accumByDate[curr.date] = updateAccum(prevAccum, curr.type)
-            return updateAccum(prevAccum, curr.type)
-          }, makeAccum(0, 0, 0, 0))
+      accumByDate[curr.date] = updateAccum(prevAccum, curr.type)
+      return updateAccum(prevAccum, curr.type)
+    }, makeAccum(0, 0, 0, 0))
 
-          return dateAndType.map(o => ({
-            date: moment(o.date),
-            total: accumByDate[o.date].total,
-            radicals: accumByDate[o.date].radicals,
-            kanjis: accumByDate[o.date].kanjis,
-            vocabulary: accumByDate[o.date].vocabulary,
-          }))
-        }
-      )
+    return dateAndType.map(o => ({
+      date: moment(o.date),
+      total: accumByDate[o.date].total,
+      radicals: accumByDate[o.date].radicals,
+      kanjis: accumByDate[o.date].kanjis,
+      vocabulary: accumByDate[o.date].vocabulary,
+    }))
   }
 
   private createChart(levelDataset: any,
                       radicalsDataset: any,
                       kanjisDataset: any,
                       vocabularyDataset: any): Chart {
-    // Calculate data bounds for the Levels dataset
-    const levelMaxY = Math.max(...levelDataset.map(({ y }) => y))
+    let datasets, yAxes
+    if (this.currentCriteria === "LEVEL") {
+      // Calculate data bounds for the Levels dataset
+      const levelMaxY = Math.max(...levelDataset.map(({ y }) => y))
+      datasets = [{
+        label: "Level Progress",
+        data: levelDataset,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 3,
+        fill: false,
+        lineTension: 0,
+        yAxisID: "left",
+      }]
+      yAxes = [{
+        id: "left",
+        display: true,
+        position: "right",
+        scaleLabel: {
+          display: true,
+          labelString: "Level"
+        },
+        ticks: {
+          max: levelMaxY + 1,
+          beginAtZero: true,
+        }
+      }]
+    } else {
+      datasets = [
+        {
+          label: "Radicals " + (this.currentCriteria === "PASSED_AT" ? "Passed" : "Burned"),
+          data: radicalsDataset,
+          backgroundColor: "rgba(0,161,241,0.2)",
+          borderColor: "rgba(0,161,241,0.8)",
+          borderWidth: 1,
+          fill: "origin",
+          pointRadius: 3,
+          steppedLine: "before",
+          yAxisID: "right",
+        },
+        {
+          label: "Kanjis " + (this.currentCriteria === "PASSED_AT" ? "Passed" : "Burned"),
+          data: kanjisDataset,
+          backgroundColor: "rgba(241,0,161,0.2)",
+          borderColor: "rgba(241,0,161,0.8)",
+          borderWidth: 1,
+          fill: "-1",
+          pointRadius: 3,
+          steppedLine: "before",
+          yAxisID: "right",
+        },
+        {
+          label: "Vocabulary " + (this.currentCriteria === "PASSED_AT" ? "Passed" : "Burned"),
+          data: vocabularyDataset,
+          backgroundColor: "rgba(161,0,241,0.2)",
+          borderColor: "rgba(161,0,241,0.8)",
+          borderWidth: 1,
+          fill: "-1",
+          pointRadius: 3,
+          steppedLine: "before",
+          yAxisID: "right",
+        }
+      ]
+      yAxes = [{
+        id: "right",
+        display: true,
+        position: "right",
+        scaleLabel: {
+          display: true,
+          labelString: "Radicals / Kanjis / Vocabulary"
+        },
+        stacked: true,
+        ticks: {
+          beginAtZero: true,
+        }
+      }]
+    }
 
     const ctx = (document.getElementById("dashboardChart") as any).getContext("2d")
     const config = {
       type: "line",
       data: {
-        datasets: [
-          {
-            label: "Level Progress",
-            data: levelDataset,
-            backgroundColor: "rgba(255, 99, 132, 0.2)",
-            borderColor: "rgba(255, 99, 132, 1)",
-            borderWidth: 3,
-            fill: false,
-            lineTension: 0,
-            yAxisID: "left",
-          },
-          {
-            label: "Radicals Passed",
-            data: radicalsDataset,
-            backgroundColor: "rgba(0,161,241,0.2)",
-            borderColor: "rgba(0,161,241,0.8)",
-            borderWidth: 1,
-            fill: "origin",
-            pointRadius: 3,
-            steppedLine: "before",
-            yAxisID: "right",
-          },
-          {
-            label: "Kanjis Passed",
-            data: kanjisDataset,
-            backgroundColor: "rgba(241,0,161,0.2)",
-            borderColor: "rgba(241,0,161,0.8)",
-            borderWidth: 1,
-            fill: "-1",
-            pointRadius: 3,
-            steppedLine: "before",
-            yAxisID: "right",
-          },
-          {
-            label: "Vocabulary Passed",
-            data: vocabularyDataset,
-            backgroundColor: "rgba(161,0,241,0.2)",
-            borderColor: "rgba(161,0,241,0.8)",
-            borderWidth: 1,
-            fill: "-1",
-            pointRadius: 3,
-            steppedLine: "before",
-            yAxisID: "right",
-          }
-        ],
+        datasets
       },
       options: {
         maintainAspectRatio: false,
@@ -218,34 +256,7 @@ export class ProgressChartComponent implements OnInit, AfterViewInit {
               }
             }
           ],
-          yAxes: [
-            {
-              id: "left",
-              display: true,
-              position: "left",
-              scaleLabel: {
-                display: true,
-                labelString: "Level"
-              },
-              ticks: {
-                max: levelMaxY + 1,
-                beginAtZero: true,
-              }
-            },
-            {
-              id: "right",
-              display: true,
-              position: "right",
-              scaleLabel: {
-                display: true,
-                labelString: "Radicals / Kanjis / Vocabulary"
-              },
-              stacked: true,
-              ticks: {
-                beginAtZero: true,
-              }
-            }
-          ]
+          yAxes
         },
         title: {
           display: true,
